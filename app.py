abbb2a18247de4c11a93f786883d4146f73f3ff5
vieiradg <diegorusty40@gmail.com>
2025-03-03 00:38:38 -0300

    treino
    aprendendo backend

---
+++ app.py
@@ -0,0 +1,18 @@
+# desafio 1
+nome = input("Qual é seu nome? ")
+print("Olá, " + nome + "." + " Prazer em te conhecer")
+
+#desafio 2
+dia = input("Qual dia vc nasceu? ")
+mes = input("Qual mês vc nasceu? ")
+ano = input("Qual ano vc nasceu? ")
+
+print("Você nasceu no dia", dia, "de " + mes + " no ano de", ano)
+
+#desafio 3
+n1 = float(input("digite um numero "))
+n2 = float(input("digite outro numero "))
+
+r = n1 + n2
+
+print("a soma de:", n1, "+", n2, "é igual a:", r)
