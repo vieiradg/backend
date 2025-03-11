a07e7fad8a6fb5c52a426d69a1f852a9d0f9b7cb
vieiradg <diegorusty40@gmail.com>
2025-03-10 23:48:31 -0300

    estudos
    listas e tuplas

---
+++ ex/Listas_Tuplas.py
@@ -0,0 +1,21 @@
+'''Listas [] (mutaveis = consegue modificar)'''
+
+#append = adicionar elementos no final da lista
+#sort = organiza em ordem alfabética, int e string não podem ser comparadas, caso o numero venha como string o número vai pra frente.
+#remove = remove algo da lista
+#count = a quantidade de ocorrências na lista 
+#insert = (posição, item) = (0, zorro) insere algo na lista de acordo com indice selecionado
+#pop = (posição, item) = (0, zorro) remove algo na lista de acordo com indice selecionado, ou remove o ultimo caso nao tenha indice no parametro
+#length do js no python é len = conta a quantidade de itens
+#index = verifica do indice do item 
+
+'''Tuplas () (imutaveis = não consegue modificar)'''
+
+#usadas quando queremos proteger um dado de modificações e ter o acesso ao dado de forma mais rápida.
+
+'''qualquer corte que seja feito no slice é usado somente para impressão'''
+#print((minha_tupla[:1])) limite de dados que iremos percorrer, começa do indice 3 
+#print((minha_tupla[1:])) excluo a quantidade de itens posteriores ao indice 
+#print((minha_tupla[3:7])) corta os itens entre o 3 e o 7
+
+#numeros = 1,2,3,4,5,6,7 #empacotamento implicito // o python sempre retorna uma tupla, lembre sempre de usar os parenteses numeros = (1,2,3,4,5,6,7)
